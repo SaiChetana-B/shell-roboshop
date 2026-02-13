3d6bb3bae780d4be10d5fa0866e30ffc23715435
sai chetana <saichetana26.com>
2026-02-13 17:41:11 +0530

    actions

--- roboshop.sh
+++ roboshop.sh
@@ -3,6 +3,9 @@
 
 SG_ID="sg-036d15f46d1ab4ac2"
 AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z00508703EZ0CC8L7C9YK"
+DOMAIN_NAME="daws88s.online"
+
 
 for instance in $@
 do
@@ -21,6 +24,7 @@ do
             --query 'Reservations[].Instances[].PublicIpAddress' \
             --output text
         )
+        RECORD_NAME="$DOMAIN_NAME" #daws88s.online
     else
         IP=$(
             aws ec2 describe-instances \
@@ -28,8 +32,34 @@ do
             --query 'Reservations[].Instances[].PrivateIpAddress' \
             --output text
         )
+        RECORD_NAME="$instance.$DOMAIN_NAME" #mongodb.daws88s.online
         fi 
 
         echo "IP Address: $IP"
+
+        aws route53 change-resource-record-sets \
+         --hosted-zone-id $ZONE_ID \
+         --change-batch '
+         {
+  "Comment": "Updating record ",
+  "Changes": [
+    {
+      "Action": "UPSERT",
+      "ResourceRecordSet": {
+        "Name": "'$RECORDNAME'",
+        "Type": "A",
+        "TTL": 1,
+        "ResourceRecords": [
+          {
+            "Value": "'$IP'"
+          }
+        ]
+      }
+    }
+  ]
+}
+'
+echo "record update for $instance"
+
 done
 
